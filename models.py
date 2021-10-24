181557ff8bfadba11d73f0be7e790c5a1e0191cb
nixbytes <real8686@gmail.com>
2021-10-24 15:58:19 -0400

    NearEarthObject

--- models.py
+++ models.py
@@ -78,7 +78,13 @@ class NearEarthObject:
         # TODO: Use this object's attributes to return a human-readable string representation.
         # The project instructions include one possibility. Peek at the __repr__
         # method for examples of advanced string formatting.
-        return f"A NearEarthObject ..."
+        #       
+        if self.hazardous:
+            self.is_hazardous = "is"
+        else:
+            self.is_hazardous = "is not"
+
+        return f"NEO {self.fullname} has a diameter of {self.diameter:.3f} km {self.is_hazardous} potentially hazardous. "
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
